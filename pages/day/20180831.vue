867fbd28919d22a777b40fcdc119a91706a18ec3
YukaKadokura <kadokura00000@gmail.com>
2018-08-31 23:13:43 +0900

    送信ボタンを押すとアラートが出るだけのボタンを実装。generateした時のパスの指定を追加。

--- pages/day/20180831.vue
+++ pages/day/20180831.vue
@@ -1,3 +1,19 @@
 <template>
-    <h1>20180831</h1>
+    <div>
+        <h1>20180831</h1>
+        <div>
+            <p>//送信ボタンを押すとアラートが出る</p>
+            <v-btn small v-on:click="onSubmitted('送信')">送信ボタン</v-btn>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    onSubmitted(message) {
+      alert('onSubmitted!：' + message);
+    },
+  },
+}
+</script>
