a919d2bb644da5b8ba001b02513fc7b26e68c634
YukaKadokura <kadokura00000@gmail.com>
2018-09-01 04:34:07 +0900

    テキストフィールドに入力した文字列がページ内に表示され、そのままその下のボタンを押すと文字列が表示されたアラートが出るよう実装。スタイルの追加。

--- pages/day/20180831.vue
+++ pages/day/20180831.vue
@@ -1,19 +1,55 @@
 <template>
-    <div>
+    <div id="content">
         <h1>20180831</h1>
-        <div>
-            <p>//送信ボタンを押すとアラートが出る</p>
-            <v-btn small v-on:click="onSubmitted('送信')">送信ボタン</v-btn>
+        <div id="content1">
+          <p>//送信ボタンを押すとアラートが出る</p>
+          <v-btn small v-on:click="onSubmitted('送信')">送信ボタン</v-btn>
+        </div>
+        <div id="content2">
+          <p>//テキストフィールドに入力すると入力した文字列が下に表示され、そのままボタンを押すとアラート内に入力した文字列が表示される</p>
+          <v-flex xs12 sm6 md3>
+            <v-text-field v-model="name" label="名前は？"></v-text-field>
+            <p>Your name： {{ name }} </p>
+            <v-btn v-on:click="getText()">テキストの送信</v-btn>
+          </v-flex>
         </div>
     </div>
 </template>
 
 <script>
 export default {
+  head: {
+    title: '20180831のページ'
+  },
+  asyncData() {
+    return {
+      name: ""
+    }
+  },
   methods: {
     onSubmitted(message) {
       alert('onSubmitted!：' + message);
     },
-  },
+    getText(){
+      alert('you inputed!：' + this.name);
+    },
+  }
 }
 </script>
+
+<style>
+#content{
+  padding: 20px;
+  
+}
+#content1{
+  margin: 5px;
+  padding: 20px;
+  border: solid;
+}
+#content2{
+  margin: 5px;
+  padding: 20px;
+  border: solid;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
+  <div id="content">
     <h1>freespace</h1>
-    <v-btn small nuxt-link to="/day/20180831">20180831</v-btn>
+    <div id="day">
+      <v-btn small nuxt-link to="/day/20180831">20180831</v-btn>
+    </div>
   </div>
 </template>
 
+<style>
+#content{
+  padding: 20px;
+}
+#day{
+  margin: 5px;
+}
+</style>
