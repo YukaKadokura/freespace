11f0c91bbca43313a4c3a79cf2d39d71e2ce0c94
YukaKadokura <kadokura00000@gmail.com>
2018-09-03 22:36:51 +0900

    indexから20180903へのリンクの指定ミスを修正。

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <v-btn small nuxt-link to="/reverselookup">Vueでやりたいこと逆引き</v-btn>
     </div>
     <div class="Contents">
-      <v-btn small nuxt-link to="/day_20180903">20180903</v-btn>
+      <v-btn small nuxt-link to="/day/20180903">20180903</v-btn>
       <v-btn small nuxt-link to="/day/20180902">20180902</v-btn>
       <v-btn small nuxt-link to="/day/20180901">20180901</v-btn>
       <v-btn small nuxt-link to="/day/20180831">20180831</v-btn>
