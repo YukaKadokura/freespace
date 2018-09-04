ca5036c41d07c93e3599cfad174a0947762b8f57
YukaKadokura <kadokura00000@gmail.com>
2018-09-04 22:38:43 +0900

    20180904のボタンの追加。

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
       <v-btn small nuxt-link to="/reverselookup">Vueでやりたいこと逆引き</v-btn>
     </div>
     <div class="Contents">
+      <v-btn small nuxt-link to="/day/20180904">20180904</v-btn>
       <v-btn small nuxt-link to="/day/20180903">20180903</v-btn>
       <v-btn small nuxt-link to="/day/20180902">20180902</v-btn>
       <v-btn small nuxt-link to="/day/20180901">20180901</v-btn>
