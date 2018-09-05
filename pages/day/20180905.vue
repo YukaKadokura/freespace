6ccd7198c017e3ed36d6cc9d62742f68e6729995
YukaKadokura <kadokura00000@gmail.com>
2018-09-05 23:49:32 +0900

    LINE風の吹き出しの作成。display:inline,inline-blockの検証。

--- pages/day/20180905.vue
+++ pages/day/20180905.vue
@@ -15,6 +15,33 @@
             <p>//配列の一番最初の要素を返却します。</p>
             <p>{{ testForDestructuringAssignment() }}</p>
         </div>
+        <div class="SubContent">
+            <p>//LINE風吹き出しです。</p>
+            <p>//わざと部品の一部を透明にしていません。</p>
+            <div id="Balloon">
+                <p>ふきだし</p>
+            </div>
+        </div>
+        <div class="SubContent">
+            <p>//display:inline; でリストの要素を横並びにします。</p>
+            <ul id="menu1">
+                <li>リスト1</li>
+                <li>リスト2</li>
+                <li>リスト3</li>
+                <li>リスト4</li>
+                <li>リスト5</li>
+            </ul>
+        </div>
+        <div class="SubContent">
+            <p>//display:inline-block; でwidthを指定したリストの要素を横並びにします。</p>
+            <ul id="menu2">
+                <li>リスト1</li>
+                <li>リスト2</li>
+                <li>リスト3</li>
+                <li>リスト4</li>
+                <li>リスト5</li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -59,5 +86,56 @@ export default{
     padding:20px;
     margin:5px;
     border:dotted;
+}
+#Balloon{
+    /* 配置方法が相対位置 */
+    position: relative;
+
+    /* 要素が生成するボックスの表示形式を横並びにする */
+    display: inline-block;
+
+    /* ボックスの幅 */
+    margin: 15px;
+	padding: 10px;
+	min-width: 200px;
+ 	max-width: 100%;
+
+    /* 色、文字 */
+    color: #000000;
+	font-size: 16px;
+    background: #1dcd00;
+    border-radius: 5px;
+}
+
+/* 選択した要素の最初の子要素として擬似要素を作成する */
+#Balloon:before{
+    content: "";
+
+    /* 配置方法が絶対位置 */
+    position: absolute;
+
+    /* 上からの配置位置の指定 */
+    top: 50%;
+
+    /* 左からの配置位置の指定 */
+    left: -30px;
+    margin-top: -15px;
+
+    /* わざと部品に使用するボーダーの色を透明にしない */
+    border: 15px solid;
+    border-right: 30px solid #1dcd00;
+}
+#menu1 li{
+    margin: 5px;
+    padding: 5px;
+    display: inline;
+    border: solid;
+}
+#menu2 li{
+    margin: 5px;
+    display: inline-block;
+    width: 100px;
+    text-align: center;
+    border: solid;
 }
 </style>
