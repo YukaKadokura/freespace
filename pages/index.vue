332c3fcea1ac153de528d2d365d6796324293563
YukaKadokura <kadokura00000@gmail.com>
2018-09-08 01:03:43 +0900

    トップページのリンクの変更。

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,14 @@
       <v-btn small nuxt-link to="/reverselookup">Vueでやりたいこと逆引き</v-btn>
     </div>
     <div class="Contents">
-      <v-btn small nuxt-link to="/day/20180907">20180907</v-btn>
-      <v-btn small nuxt-link to="/day/20180906">20180906</v-btn>
-      <v-btn small nuxt-link to="/day/20180905">20180905</v-btn>
-      <v-btn small nuxt-link to="/day/20180904">20180904</v-btn>
-      <v-btn small nuxt-link to="/day/20180903">20180903</v-btn>
-      <v-btn small nuxt-link to="/day/20180902">20180902</v-btn>
-      <v-btn small nuxt-link to="/day/20180901">20180901</v-btn>
-      <v-btn small nuxt-link to="/day/20180831">20180831</v-btn>
+      <v-btn small nuxt-link to="/day/2018/09/07">20180907</v-btn>
+      <v-btn small nuxt-link to="/day/2018/09/06">20180906</v-btn>
+      <v-btn small nuxt-link to="/day/2018/09/05">20180905</v-btn>
+      <v-btn small nuxt-link to="/day/2018/09/04">20180904</v-btn>
+      <v-btn small nuxt-link to="/day/2018/09/03">20180903</v-btn>
+      <v-btn small nuxt-link to="/day/2018/09/02">20180902</v-btn>
+      <v-btn small nuxt-link to="/day/2018/09/01">20180901</v-btn>
+      <v-btn small nuxt-link to="/day/2018/08/31">20180831</v-btn>
     </div>
   </div>
 </template>
