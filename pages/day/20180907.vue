9bf93724c2d5e8970e714307ffcaaf19f2ddccb5
YukaKadokura <kadokura00000@gmail.com>
2018-09-07 23:24:16 +0900

    サイドメニューとパンくずリストの雰囲気を実装（微妙）。

--- pages/day/20180907.vue
+++ pages/day/20180907.vue
@@ -1,44 +1,81 @@
 <template>
-    <div class="MainContent">
-        <h1>20180907</h1>
-        <div class="SubContent">
-            <p>//CSSで丸を作ります。</p>
-            <div class="circles flex">
-                <div class="circle circle1">
-                    円です。
-                </div>
-                <div class="circle circle2">
-                    文字を中央に寄せた円です。
-                </div>
-                <div class="circle circle3">
-                    枠線の円です。
+    <div class="content flex">
+        <div class="menu">
+            <ul>
+                <li>トップページ</li>
+                <li>日々の記録</li>
+                <li>Vue逆引き</li>
+            </ul>
+        </div>
+        <div class="MainContent">
+            <p class="pankuzu">トップページ > 日々の記録 > 20180907</p>
+            <h1>20180907</h1>
+            <div class="SubContent">
+                <p>//CSSで丸を作ります。</p>
+                <div class="circles flex">
+                    <div class="circle circle1">
+                        円です。
+                    </div>
+                    <div class="circle circle2">
+                        文字を中央に寄せた円です。
+                    </div>
+                    <div class="circle circle3">
+                        枠線の円です。
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="SubContent">
-            <p>//文字を縦書きにします。</p>
-            <div class="flex">
-                <p class="vertical">あいうえお</p>
-                <p class="vertical">かきくけこ</p>
-                <p class="vertical">さしすせそ</p>
+            <div class="SubContent">
+                <p>//文字を縦書きにします。</p>
+                <div class="flex">
+                    <p class="vertical">あいうえお</p>
+                    <p class="vertical">かきくけこ</p>
+                    <p class="vertical">さしすせそ</p>
+                </div>
             </div>
-        </div>
-        <div class="SubContent">
-            <p>//HTMLのdetails、summary要素の検証です。</p>
-            <details>
-                <summary>
-                    概要はsummaryタグで囲い、summaryタグをdetailsタグで囲みます。
-                </summary>
-                詳細な内容はsummaryタグで囲まないdetailsタグの内部に書きます。
-            </details>
+            <div class="SubContent">
+                <p>//HTMLのdetails、summary要素の検証です。</p>
+                <details>
+                    <summary>
+                        概要はsummaryタグで囲い、summaryタグをdetailsタグで囲みます。
+                    </summary>
+                    詳細な内容はsummaryタグで囲まないdetailsタグの内部に書きます。
+                </details>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
+html{
+    height:100%;
+}
+body{
+    height:100%;
+    margin:0;
+}
+.content{
+    background:#bc8f8f;
+}
+.menu{
+    margin:0;
+    background:#48d1cc;
+}
+.menu li{
+    font-weight:bold;
+    color:#ffffff;
+    border: none;
+
+    display:block;
+    padding:15px 30px 15px 15px;
+    list-style:none;
+}
 .MainContent{
     margin:20px;
     padding:20px;
+    background:#ff0000;
+}
+.pankuzu{
+    font-size:11px;
 }
 .SubContent{
     margin:5px;
@@ -47,6 +84,8 @@
 }
 .flex{
     display:flex;
+    height:100%;
+    width:100%;
 }
 .circle{
     margin:5px;
