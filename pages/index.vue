816ccc01255492b76ea79eca001cd9392510c482
YukaKadokura <kadokura00000@gmail.com>
2018-09-09 01:55:19 +0900

    トップページのスタイルの修正。

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,46 @@
 <template>
-  <div class="MainContent">
-    <h1>freespace</h1>
-    <div class="Contents">
+  <div class="container">
+    <h1 class="title">freespace</h1>
+    <div class="content">
       <v-btn small nuxt-link to="/reverselookup">Vueでやりたいこと逆引き</v-btn>
     </div>
-    <div class="Contents">
-      <v-btn small nuxt-link to="/day/2018/09/09">20180909</v-btn>
-      <v-btn small nuxt-link to="/day/2018/09/08">20180908</v-btn>
-      <v-btn small nuxt-link to="/day/2018/09/07">20180907</v-btn>
-      <v-btn small nuxt-link to="/day/2018/09/06">20180906</v-btn>
-      <v-btn small nuxt-link to="/day/2018/09/05">20180905</v-btn>
-      <v-btn small nuxt-link to="/day/2018/09/04">20180904</v-btn>
-      <v-btn small nuxt-link to="/day/2018/09/03">20180903</v-btn>
-      <v-btn small nuxt-link to="/day/2018/09/02">20180902</v-btn>
-      <v-btn small nuxt-link to="/day/2018/09/01">20180901</v-btn>
-      <v-btn small nuxt-link to="/day/2018/08/31">20180831</v-btn>
+    <div class="content">
+      <div class="day">
+        <p>201809</p>
+        <v-btn small nuxt-link to="/day/2018/09/09">20180909</v-btn>
+        <v-btn small nuxt-link to="/day/2018/09/08">20180908</v-btn>
+        <v-btn small nuxt-link to="/day/2018/09/07">20180907</v-btn>
+        <v-btn small nuxt-link to="/day/2018/09/06">20180906</v-btn>
+        <v-btn small nuxt-link to="/day/2018/09/05">20180905</v-btn>
+        <v-btn small nuxt-link to="/day/2018/09/04">20180904</v-btn>
+        <v-btn small nuxt-link to="/day/2018/09/03">20180903</v-btn>
+        <v-btn small nuxt-link to="/day/2018/09/02">20180902</v-btn>
+        <v-btn small nuxt-link to="/day/2018/09/01">20180901</v-btn>
+      </div>
+      <div class="day">
+        <p>201808</p>
+        <v-btn small nuxt-link to="/day/2018/08/31">20180831</v-btn>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.MainContent{
-  padding: 20px;
+.container{
+  margin:20px;
+  padding-top: 10px;
 }
-.Contents{
-  margin: 5px;
+.content{
+  margin-top: 10px;
+  padding:5px;
+}
+.title{
+  color:#202020;
+}
+.day{
+  margin-top:10px;
+}
+.day p{
+  font-size:13px;
 }
 </style>
