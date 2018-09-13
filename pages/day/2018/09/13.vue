cb46dafdcd1b891e49b2c60bdf8e670ebe1c5b42
YukaKadokura <kadokura00000@gmail.com>
2018-09-13 23:52:21 +0900

    20180913分のページの作成。

---
+++ pages/day/2018/09/13.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <div class="circle">
+        まる
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            title:"20180913"
+        }
+    }
+}
+</script>
+
+<style scoped>
+.circle{
+    width:100px;
+    height:100px;
+    padding:30px;
+    border-radius:50%;
+    background:skyblue;
+}
+</style>
